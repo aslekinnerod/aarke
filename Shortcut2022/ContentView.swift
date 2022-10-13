c8864f0a7f3c44259b1eab17f5d6e8622175b613
Asle Kinnerød <asle.b.kinnerod@shortcut.no>
2022-10-13 09:31:42 +0200

    Initial Commit

---
+++ Shortcut2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shortcut2022
+//
+//  Created by Asle Kinnerød on 13/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shortcut2022/Shortcut2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Shortcut2022App.swift
+//  Shortcut2022
+//
+//  Created by Asle Kinnerød on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Shortcut2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
