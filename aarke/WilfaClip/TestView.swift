2f9567cc3e88fe908bba5990b5f2bece8edd5b88
Asle Kinnerød <asle78@gmail.com>
2023-11-06 14:29:43 +0100

    latest changes

---
+++ aarke/WilfaClip/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  aarke
+//
+//  Created by Asle Kinnerød on 25/10/2023.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
