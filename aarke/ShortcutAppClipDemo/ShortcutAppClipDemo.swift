ac1f2ed6e89e0c0e0a3a375d380fd4ab89877c50
Eric <eric36davis@icloud.com>
2023-11-09 12:53:41 +0100

    Initial commit of shortcut app clip white label

---
+++ aarke/ShortcutAppClipDemo/ShortcutAppClipDemo.swift
@@ -0,0 +1,65 @@
+//
+//  WilfaClipApp.swift
+//  WilfaClip
+//
+//  Created by Asle Kinnerød on 21/11/2022.
+//
+
+import SwiftUI
+
+struct BrandInfo {
+    private let assetFolderName: String
+    let name: String
+
+    init(assetFolderName: String, name: String) {
+        self.assetFolderName = assetFolderName
+        self.name = name
+    }
+
+    var headerImage: Image {
+        Image("\(assetFolderName)/headerImage")
+    }
+}
+enum Brand: String, CaseIterable {
+    case porsche = "/porsche"
+    case bmw = "/bmw"
+    case shortcut = "/shortcut"
+
+    var info: BrandInfo {
+        switch self {
+        case .porsche:
+            return BrandInfo(assetFolderName: "porsche", name: "Porsche")
+        case .bmw:
+            return BrandInfo(assetFolderName: "bmw", name: "BMW")
+        case .shortcut:
+            return BrandInfo(assetFolderName: "shortcut", name: "Shortcut")
+        }
+    }
+}
+
+@main
+struct ShortcutDemoAppClip: App {
+    @State var brand: Brand?
+
+    var body: some Scene {
+        WindowGroup {
+            Group {
+                if let brand {
+                    ContentView(brand: brand)
+                } else {
+                    ProgressView()
+                }
+            }
+            .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { userActivity in
+                guard let incomingURL = userActivity.webpageURL else { return }
+
+                print(incomingURL)
+                for brand in Brand.allCases {
+                    if brand.rawValue == incomingURL.path() {
+                        self.brand = brand
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ aarke/ShortcutAppClipDemo/Usermanual/UsermanualView.swift
@@ -0,0 +1,53 @@
+//
+//  UsermanualView.swift
+//  WilfaClip
+//
+//  Created by Asle Kinnerød on 23/11/2022.
+//
+
+import SwiftUI
+
+struct UsermanualView: View {
+  
+    
+    var body: some View {
+        VStack{
+            TopBanner()
+                .padding(.bottom, -55)
+            HeadingView(title: "BRUKSANVISNING")
+                .padding(.bottom, 20)
+            NavigationLink(destination: ProductOverView()){
+                ManualButtonView(title: "Förpackningens innehåll")
+            }
+            .navigationTitle("")
+            
+            NavigationLink(destination: BeforeUseView()){
+                ManualButtonView(title: "Produktens komponenter")
+            }
+            .navigationTitle("")
+            
+            NavigationLink(destination: UserguideView()){
+                ManualButtonView(title: "Installation av kollsyrepatron")
+            }
+            .navigationTitle("")
+            
+            NavigationLink(destination: CleaningView()){
+                ManualButtonView(title: "Kolsyra vatten")
+            }
+            .navigationTitle("")
+            
+          /*  NavigationLink(destination: ProblemSolvingView()){
+                ManualButtonView(title: "Problemlösing")
+            }
+            .navigationTitle("")*/
+            Spacer()
+                .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+struct UsermanualView_Previews: PreviewProvider {
+    static var previews: some View {
+        UsermanualView()
+    }
+}
